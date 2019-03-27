11535a89a87cffc225a88268c71dda8e05d938a7
David Ståhl <47720480+DavidnStahl@users.noreply.github.com>
2019-03-27 16:44:33 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reversedString2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Write a sentence: ");
+            string sentence = Console.ReadLine();
+            string[] words = sentence.Split(' '); // vid varje mellanslag delas orden in multiarray
+            for (int i = words.Length - 1; i >= 0; i--)
+            {
+                Console.Write(words[i] + " "); // Ge varje ord ett mellanslag i loopen
+
+            }
+
+            Console.ReadLine();
+    }   }
+}
